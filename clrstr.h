8dc55cea07ba44b1497bbf0aa4f7dc7ba35e70ca
anyway <zorix228337@gmail.com>
2026-02-15 22:55:53 +0300

    add color's enumerator

--- clrstr.h
+++ clrstr.h
@@ -9,14 +9,24 @@ extern "C" {
 
 #elif defined(WIN32) || defined(WIN64)
 #include <windows.h>    // in the future I want to replace this
-                        // lib with something which don't need
-                        // this lib
+// lib with something which don't need
+// this lib
 
-typedef unsigned color;
+typedef unsigned color_n;
+
+typedef enum : color_n {
+    RED =       FOREGROUND_RED | FOREGROUND_INTENSITY,
+    GREEN =     FOREGROUND_GREEN | FOREGROUND_INTENSITY,
+    BLUE =      FOREGROUND_BLUE | FOREGROUND_INTENSITY,
+    WHEEL =     FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_INTENSITY,
+    PURPLE =    FOREGROUND_BLUE | FOREGROUND_RED | FOREGROUND_INTENSITY,
+    YELLOW =    FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_INTENSITY,
+    WHITE =     FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE,
+} color;
 
 typedef struct {
     color clr;
-    char* txt;
+    char *txt;
 } clr_txt_t;
 
 #ifdef __cplusplus
