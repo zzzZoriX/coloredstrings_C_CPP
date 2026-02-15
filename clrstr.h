1a6898c5ad9da382d70093b8ebd3a6a5da476bbe
anyway <zorix228337@gmail.com>
2026-02-15 23:16:16 +0300

    add API functions headers

--- clrstr.h
+++ clrstr.h
@@ -9,8 +9,8 @@ extern "C" {
 
 #elif defined(WIN32) || defined(WIN64)
 #include <windows.h>    // in the future I want to replace this
-// lib with something which don't need
-// this lib
+                        // lib with something which don't need
+                        // this lib
 
 typedef unsigned color_n;
 
@@ -28,6 +28,29 @@ typedef struct {
     color clr;
     char *txt;
 } clr_txt_t;
+
+/**
+ * @brief print single clr_txt_t structure object
+ *
+ * @param clrtxto   object which need print
+ *
+ * @return nothing
+ */
+void __cdecl
+clrstr_print_single(clr_txt_t clrtxto);
+
+/**
+ * @brief print each clr_txt_t structre object in given array
+ *
+ * @param clrtxtos  array of clr_txt_t structure objects
+ * @param count     total count of objects in array
+ */
+void __cdecl
+clrstr_print(clr_txt_t clrtxtos, size_t count);
+
+
+#define DEFAULT_CLRTXT (clr_txt_t){WHITE, ""};
+#define MAKE_CLRTXT(clr, txt) (clr_txt_t){(clr), (txt)}
 
 #ifdef __cplusplus
 }
